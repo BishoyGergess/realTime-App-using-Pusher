f282f40ae6413f128f39ddabe550db2ffaa86b78
Bishoy Milad <bishoyanwar@outlook.com>
2019-03-08 22:48:14 +0200

    ORM's

--- app/Question.php
+++ app/Question.php
@@ -9,4 +9,16 @@ class Question extends Model
     protected $fillable = [
         'title','body', 'slug','user_id','category_id'
     ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function categories()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
--- app/Reply.php
+++ app/Reply.php
@@ -9,4 +9,16 @@ class Reply extends Model
     protected $fillable = [
         'body','user_id', 'question_id'
     ];
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function like()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
