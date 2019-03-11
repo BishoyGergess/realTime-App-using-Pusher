3917a7f12a10e36c0a966d83f29f0339f316aa1a
Bishoy Milad <bishoyanwar@outlook.com>
2019-03-11 20:01:15 +0200

    jwt adding

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,17 @@ use Illuminate\Http\Request;
  Route::apiResource('/question/{question}/reply','ReplyController');
  Route::post('/like/{reply}','LikeController@likeIt');
  Route::delete('/like/{reply}','LikeController@unLikeIt');
+
+ Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
