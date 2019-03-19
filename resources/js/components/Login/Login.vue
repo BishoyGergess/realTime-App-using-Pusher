289d125b7be6701b90ff0531788396613cbc2da7
Bishoy Milad <bishoyanwar@outlook.com>
2019-03-19 16:03:52 +0200

    Login

---
+++ resources/js/components/Login/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-form @submit.prevent="login">
+    <v-container>
+      <v-layout>
+        <v-flex
+          xs12
+          md12
+        >
+          <v-text-field
+            v-model="form.email"
+            label="E-mail"
+            type="email"
+            required
+          ></v-text-field>
+        </v-flex>
+                <v-flex
+          xs12
+          md12
+        >
+          <v-text-field
+            v-model="form.password"
+            label="Password"
+            type="password"
+            required
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+
+    <v-btn
+    color="Green"
+    type="submit"
+    >Login</v-btn>
+    </v-container>
+  </v-form>
+</template>
+
+
+<script>
+  export default {
+      data(){
+          return{
+              form:{
+                  email:null,
+                  password:null
+              }
+          }
+        },
+        methods:{
+            login(){
+                axios.post('/api/auth/login',this.form).
+                then(res=>console.log(res.data))
+                .catch(error=>console.log(error.response.data));
+
+
+                }
+            }
+    }
+</script>
+
+<style>
+
+</style>
